636a0dff4b5705e677f5d3e61623207b1502cd15
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2024-02-05 12:40:18 -0500

    Initial Commit

---
+++ C867_PA/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C867_PA
+//
+//  Copyright © 2024 TaeVon Lewis. All rights reserved.
+//
+
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
